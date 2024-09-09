eaa734bc11f40b6659fdc79c44e4e8eb7d2ab3b5
Sumit Singwal <sumitsingwal@192.168.1.173>
2024-09-09 18:24:36 +0000

    Sync LeetCode submission - Duplicate Emails (mysql)

---
+++ my-folder/problems/duplicate_emails/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+#SELECT email, COUNT(email) as Total FROM Person GROUP BY email
+SELECT email as Email
+FROM (SELECT email, COUNT(email) as Total FROM Person GROUP BY email) AS E1
+WHERE Total > 1
