cb327d7d98f5bcd6b74820ec82cc3509da12f7de
Sumit Singwal <88982236+sumitsingwal@users.noreply.github.com>
2024-09-14 10:29:54 +0100

    Update Rising_Temperature.sql

--- Rising_Temperature.sql
+++ Rising_Temperature.sql
@@ -3,3 +3,9 @@
 SELECT w2.id
 FROM Weather w2
 WHERE w2.recordDate > (SELECT w1.recordDate FROM Weather w1 WHERE DATE_ADD(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate AND w2.temperature > w1.temperature);
+
+#Solution 2
+
+SELECT w2.id
+FROM Weather w2, Weather w1
+WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1 AND w2.temperature > w1.temperature;
