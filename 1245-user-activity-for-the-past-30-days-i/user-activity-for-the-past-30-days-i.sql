a0656367d90ef2a19790a4daff744246fb3e49ef
Sumit Singwal <88982236+sumitsingwal@users.noreply.github.com>
2025-10-08 19:47:22 +0100

    Time: 386 ms (43.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT activity_date as day, COUNT(DISTINCT(user_id)) as active_users
+FROM Activity
+WHERE activity_date < '2019-07-28' AND activity_date > '2019-06-27'
+GROUP BY activity_date
